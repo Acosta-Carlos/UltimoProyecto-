96776c9965df4de3d9a9644d6c914a86e66c20d1
Sebastian_AG <subkei@gmail.com>
2020-07-06 18:54:03 -0400

    Agregados controladores, Dao y beans para Cliente, Empleado, Pago.

---
+++ UltimoProyectoSpring/src/main/java/com/ultimoproyecto/controlador/EmpleadoControlador.java
@@ -0,0 +1,60 @@
+package com.ultimoproyecto.controlador;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ultimoproyecto.beans.Empleado;
+import com.ultimoproyecto.dao.EmpleadoDao;
+
+@Controller
+public class EmpleadoControlador {
+
+	@Autowired
+	EmpleadoDao daoemp;
+	
+    @RequestMapping("/empform")    
+    public String showform(Model m){    
+        m.addAttribute("command", new Empleado());  
+        return "empform";
+    } 
+	
+    @RequestMapping(value="/save",method = RequestMethod.POST)    
+    public String save(@ModelAttribute("emp") Empleado emp){    
+        daoemp.save(emp);
+        return "redirect:/viewemp";
+    }
+    
+    @RequestMapping("/viewemp")    
+    public String viewemp(Model m){
+        List<Empleado> list=daoemp.getEmpleados();
+        m.addAttribute("list",list);  
+        return "viewemp";
+    }	
+    
+    @RequestMapping(value="/editemp/{idempleado}")    
+    public String edit(@PathVariable int idempleado, Model m){    
+        Empleado emp=daoemp.getEmpById(idempleado);
+        m.addAttribute("command",emp);
+        return "empditform";
+    }
+
+    @RequestMapping(value="/editsave",method = RequestMethod.POST)    
+    public String editsave(@ModelAttribute("emp") Empleado emp){
+        daoemp.update(emp);
+        return "redirect:/viewemp";
+    }
+
+    @RequestMapping(value="/deleteemp/{idempleado}",method = RequestMethod.GET)    
+    public String delete(@PathVariable int idempleado){    
+        daoemp.delete(idempleado);    
+        return "redirect:/viewemp";
+    }
+    
+}
